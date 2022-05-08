2691f9f2ae518515d55285a6cb0477df3e3031c1
Swisskill <wbrant@uwyo.edu>
2022-05-08 15:18:04 -0600

    cleaned up the file

--- detector/detector.go
+++ detector/detector.go
@@ -1,6 +1,6 @@
 /*
 Authors: Tanner Selvig, Will Brant
-Desc: Takes a file and returns it as a byte array
+Desc: Validates if the file contains a payload
 */
 
 package main
@@ -9,7 +9,6 @@ import (
 	"bufio"
 	"bytes"
 	"encoding/binary"
-	"errors"
 	"fmt"
 	"os"
 )
@@ -17,7 +16,9 @@ import (
 func main() {
 
 	var filename string
-	fmt.Println("Heyo. To test a png for code payloads, provide the name of a png, such as test.png")
+	fmt.Println("---------------------------------------------------------------------")
+	fmt.Println("This program validates whether a file contains a payload")
+	fmt.Println("Please enter in a png file for testing (ex: test.png or payload.png)\n")
 	fmt.Scanln(&filename)
 	fd, err := os.Open(filename)
 	if err != nil {
@@ -46,58 +47,57 @@ func main() {
 
 	chunkType := ""
 	malPNG := false
-	//count := 0
-	//nothing := 0
-	for chunkType != "IEND" { //this check does not appear to work. Runs forever
+
+	for chunkType != "IEND" {
 		var chunkSize uint32
 		err := binary.Read(byteReader, binary.BigEndian, &chunkSize)
-		if err != nil {panic(err)}
+		if err != nil {
+			panic(err)
+		}
 		var chunkType = make([]byte, 4)
 		err = binary.Read(byteReader, binary.BigEndian, chunkType)
-		if err != nil {panic(err)}
+		if err != nil {
+			panic(err)
+		}
 		if string(chunkType[0:4]) == "IEND" {
 			break
 		}
 		firstDataBuf := make([]byte, 2)
 		err = binary.Read(byteReader, binary.BigEndian, firstDataBuf)
-		byteReader.Seek(int64(chunkSize) - 2, 1)
+		byteReader.Seek(int64(chunkSize)-2, 1)
 		byteReader.Seek(4, 1)
-		
-		//^this decodes the string. Are there header bytes in need to ignore
+
 		if err != nil {
 			panic(err)
-			//fmt.Println("not a prob")
+
 		} else {
-			//fmt.Println(string(bs))
-			if(!validateBytes(firstDataBuf)) {
+			if !validateBytes(firstDataBuf) {
 				malPNG = true
-				break;
+				break
 			}
-			
+
 		}
-		//fmt.Println(string(bs))
-		
 	}
 	if malPNG == true {
-		fmt.Println("this is a payload")
+		fmt.Println("This file may contain a payload. Careful!")
+		fmt.Println("----------------------------------------------")
 	} else {
-		fmt.Println("This png is safe")
+		fmt.Println("This png is safe!")
+		fmt.Println("----------------------------------------------")
 	}
 }
 
 func validateBytes(firstBytes []byte) bool {
-	// fmt.Printf("First byte: %c, Second: %c\n", firstBytes[0], firstBytes[1])
 	// There is a chance that we will get false positives when we use this method but there
 	// is really no other good way to identify an executable file
 	if firstBytes[0] == 'M' && firstBytes[1] == 'Z' { // Executable files start with MZ in their header
-		// fmt.Printf("Made it inside\n")
 		return false
 	}
 	return true
 }
 
+//for future iterations, pattern matching functions such as this can be used to further validate
 func validate(sample string) bool {
-	//decode chunk to string
 	check := true
 	for _, j := range sample {
 		switch j {
@@ -110,12 +110,4 @@ func validate(sample string) bool {
 		}
 	}
 	return check
-}
-
-// uInt32ToInt converts a 4 byte big-endian buffer to int.
-func uInt32ToInt(buf []byte) (int, error) {
-	if len(buf) == 0 || len(buf) > 4 {
-		return 0, errors.New("invalid buffer")
-	}
-	return int(binary.BigEndian.Uint32(buf)), nil
 }
